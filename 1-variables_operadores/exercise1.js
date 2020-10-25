c8c448ef9158e86530d4251a40238ec5a1b1ccf1
Valentín Silvestri <valentinsilvestri@outlook.com>
2020-10-25 18:27:14 -0300

    First commit

---
+++ 1-variables_operadores/exercise1.js
@@ -0,0 +1,17 @@
+// Exercise 1 - Variables y operadores
+
+/*
+    a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en 
+    una 3er variable.
+*/
+
+
+/*
+    b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+*/
+
+
+/*
+    c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando 
+    el resultado la suma una 3er variable (utilizar length).
+*/
