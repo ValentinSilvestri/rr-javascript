d213a6ffa1d16cba61d7576ef1de2a0cfb34bcb8
Valentín Silvestri <valentinsilvestri@outlook.com>
2020-10-26 10:47:39 -0300

    exercise 1 updated

--- 1-variables_operadores/exercise1.js
+++ 1-variables_operadores/exercise1.js
@@ -9,6 +9,7 @@ var a, b, c;
 a = 10;
 b = 15;
 c = a + b;
+console.log("Ejercicio a) Suma: " + c);
 
 /*
     b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
@@ -18,6 +19,7 @@ var str1, str2, str3;
 str1 = "Hola ";
 str2 = "Mundo";
 str3 = str1 + str2;
+console.log("Ejercicio b) Concatenación: " + str3);
 
 /*
     c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando 
@@ -28,3 +30,4 @@ var str4, str5, len;
 str4 = "String1";
 str5 = "String2";
 len = str4.length + str5.length;
+console.log("Ejercicio c) Length: " + len);
